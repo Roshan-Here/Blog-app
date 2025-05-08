d299a461976b6a1e5c86b0bbb7143e3dcf706bcf
Roshan-Here <leoparmigiana@gmail.com>
2025-05-09 03:03:57 +0530

    AnFix

--- blog-backend/utils/db.js
+++ blog-backend/utils/db.js
@@ -2,11 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDb = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONG0_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    const conn = await mongoose.connect(process.env.MONG0_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
+    return conn;
   } catch (error) {
     console.error('MongoDB connection failed:', error.message);
     process.exit(1);
