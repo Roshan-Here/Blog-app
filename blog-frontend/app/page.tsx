226b039ee696ccd82cf79ae66e10b6d2b4590d02
Roshan-Here <leoparmigiana@gmail.com>
2025-05-08 21:57:09 +0530

    frontend intialized with taiwlindcss

---
+++ blog-frontend/app/page.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function Home() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-300">
+      <p className="">Simple Blog App</p>
+    </div>
+  );
+}
